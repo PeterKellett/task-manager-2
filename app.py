a83bb6aa2953f3ed1a21e6838714d4ebe92772f5
PeterKellett <peterwkellett@gmail.com>
2020-07-01 18:39:38 +0000

    Tutorials complete and env.py set in .gitignore file

--- app.py
+++ app.py
@@ -6,8 +6,9 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = 'mongodb+srv://root:<password>@myfirstcluster-jraod.mongodb.net/task_manager?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
@@ -57,6 +58,42 @@ def delete_task(task_id):
     mongo.db.tasks.remove({'_id': ObjectId(task_id)})
     return redirect(url_for('get_tasks'))
 
+
+@app.route("/get_categories")
+def get_categories():
+    return render_template('categories.html', categories=list(mongo.db.categories.find()))
+
+
+@app.route("/edit_category/<category_id>")
+def edit_category(category_id):
+    return render_template('editcategory.html', category=mongo.db.categories.find_one({'_id': ObjectId(category_id)}))
+
+
+@app.route("/update_category/<category_id>", methods=["POST"])
+def update_category(category_id):
+    mongo.db.categories.update(
+        {'_id': ObjectId(category_id)},
+        {'category_name': request.form.get('category_name')})
+    return redirect(url_for('get_categories'))
+
+
+@app.route("/delete_category/<category_id>")
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
+@app.route("/add_category")
+def add_category():
+    return render_template('addcategory.html')
+
+
+@app.route("/insert_category", methods=['POST'])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {'category_name': request.form.get('category_name')}
+    categories.insert_one(category_doc)
+    return redirect(url_for('get_categories'))
 
 
 if __name__ == '__main__':
